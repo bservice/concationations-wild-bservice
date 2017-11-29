f6b81a13c2bc7c4426fc76b795576956b8c58ffb
bs666 <brservice@comcast.net>
2017-11-29 08:31:34 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String args[]) {
+        String t1 = cat(68, 108);
+        System.out.println(t1);
+
+        String t2 = cat(35, 59);
+        System.out.println(t2);
+    }
+    private static String cat(int start, int end) {
+        StringBuffer sb = new StringBuffer();
+        char h;
+        for (int i = start; i <= end; i++){
+            h = (char)i;
+            sb.append(h);
+        }
+        return sb.toString();
+    }
+}
